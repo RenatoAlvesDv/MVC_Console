ad8619dc3a3cd1fea5d115625793156b97d533e4
Your Name <renato_inovacao@hotmail.com>
2021-01-05 10:46:27 -0300

    Primeira parte do Projeto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MVC_Console.Controllers;
+using MVC_Console.Models;
+
+namespace MVC_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instaciamos nosso Controller
+            ProdutoController produtoController = new ProdutoController();
+            produtoController.MostrarProdutos();
+
+        }
+    }
+}
